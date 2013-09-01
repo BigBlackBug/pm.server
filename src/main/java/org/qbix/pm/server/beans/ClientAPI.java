749a1ddccb7a0b229253c6ef105397b3c1f1238a
iliax <iliax@iliax-pc>
2013-09-01 18:41:15 +0400

    Добавил описание json параметров при вызове методов

--- src/main/java/org/qbix/pm/server/beans/ClientAPI.java
+++ src/main/java/org/qbix/pm/server/beans/ClientAPI.java
@@ -14,12 +14,23 @@ import org.qbix.pm.server.exceptions.PMException;
 @Path("/")
 public interface ClientAPI {
 
+	// http://localhost:8080/pm.server/rs/regsess
+	// {
+	// "stake" : 200.0,
+	// "vc" : { "parserId" : 0 },
+	// "type" : "LOL",
+	// "playerInfos" : [
+	// {"accountId" : 1} , {"accountId" : 2, "stake" : 200 }
+	// ]
+	// }
 	@POST
 	@Path("/regsess")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
 	public Long registerSession(SessionInfo si) throws PMException;
 
+	// http://localhost:8080/pm.server/rs/confpart
+	// { "sessid" : 249, "accountid" : 1 }
 	@POST
 	@Path("/confpart")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -30,15 +41,18 @@ public interface ClientAPI {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void cancelParticipation(UserJoinInfo uji) throws PMException;
 
+	// http://localhost:8080/pm.server/rs/startsess
+	// { "sessid" : 249 }
 	@POST
 	@Path("/startsess")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void startSession(SessionInfo si) throws PMException;
-	
-	/** if session requires manual start command */
+
+	// http://localhost:8080/pm.server/rs/resolveresult
+	// { "sessid" : 258, "winner" : 0 }
 	@POST
 	@Path("/resolveresult")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void resolveResult(ResultInfo si) throws PMException;
-	
+
 }
